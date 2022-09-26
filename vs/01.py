47165ef96f066697d0734b31bd0fcb9045d7b26a
Pablo Kaniefsky <pablokan@gmail.com>
2022-09-26 14:24:28 -0300

    pre first class

--- vs/01.py
+++ vs/01.py
@@ -1,19 +1,8 @@
 from PySide6.QtWidgets import QApplication, QWidget
 
-# Only needed for access to command line arguments
-import sys
+app = QApplication() # crea una instancia de la clase Aplicación 
+window = QWidget() # crea una ventana como widget 
+# (todos los objetos que veremos son widgets, los botones por ejemplo)
+window.show() # muestra la ventana
+app.exec() # inicia el bucle de eventos
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
-
-# Create a Qt widget, which will be our window.
-window = QWidget()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-
-# Start the event loop.
-app.exec_()
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.
