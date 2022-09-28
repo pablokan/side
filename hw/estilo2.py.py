8cbee188d8321ff366f51a04dab0d609e805b13f
Pablo Kaniefsky <pablokan@gmail.com>
2022-09-28 01:24:26 -0300

    estilo2

--- hw/estilo2.py.py
+++ hw/estilo2.py.py
@@ -1,12 +1,16 @@
-from turtle import color
+# ta fallido por ahora, capaz no tiene arreglo igual
+# estilo.py funciona bien pero duplica código
+
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
 from PySide6.QtGui import QFont
 from PySide6.QtCore import Qt
 
 
-class Style(QWidget):
+class Style():
     def __init__(self, Class, text="", fontF="Arial", fontS=15, fore="black", 
                 back="white", horizontalAlign="hC", verticalAlign="vC"):
+        Class.__init__(Class(), text="", fontF="Arial", fontS=15, fore="black", 
+                back="white", horizontalAlign="hC", verticalAlign="vC")
         align = {
             "aL": Qt.AlignLeft,
             "hC": Qt.AlignCenter,
@@ -15,7 +19,7 @@ class Style(QWidget):
             "vC": Qt.AlignVCenter,
             "aB": Qt.AlignBottom
             }
-        Class.setText(str(text))
+        self.setText(str(text))
         self.setStyleSheet(f'background-color: {back}; color: {fore};')
         self.setAlignment(align[horizontalAlign] | align[verticalAlign])
         self.setFont(QFont(fontF, fontS))
@@ -28,12 +32,12 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
 
         # layout.addWidget(Style(QLabel, "pablo", "Victor Mono", 10, "sky blue", "blue", "aR", "aT"))
-        # layout.addWidget(Style(QLabel, "kan", "Victor Mono", 30, "red", "orange", "aL", "aB"))
+        #layout.addWidget(Style(QLabel, "kan", "Victor Mono", 30, "red", "orange", "aL", "aB"))
         # layout.addWidget(Style(QLabel, "Manchester City"))
-        # layout.addWidget(Style(QLineEdit, fore="red", back="yellow"))
-        # layout.addWidget(Style(QLabel, "Liverpool", horizontalAlign="aR"))
+        #layout.addWidget(Style(QLineEdit, fore="red", back="yellow"))
+        layout.addWidget(Style(QLabel, "Liverpool", horizontalAlign="aR"))
         # layout.addWidget(Style(QLabel, "lo mismo"))
-        layout.addWidget(Style(QLineEdit, "lo mismo"))
+        # layout.addWidget(Style(QLineEdit, "lo mismo"))
 
         centralWidget = QWidget()
         centralWidget.setLayout(layout)
