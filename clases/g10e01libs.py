bd1b321b708fa8cc175d483e0e270e57ca06ebfa
Pablo Kaniefsky <pablokan@gmail.com>
2022-09-29 05:54:36 -0300

    w1 a

---
+++ clases/g10e01libs.py
@@ -0,0 +1,38 @@
+import os, sys
+sys.path.append(os.path.abspath('libs'))
+
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout
+from styles import Text, Input, Button
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        self.texto = Text('Ingrese 2 números', fontF="NovaMono", fontS=30, fore="red", back="yellow")
+        layout.addWidget(self.texto)
+        self.numero1 = Input(fontS=40, fore="magenta", back="black")
+        layout.addWidget(self.numero1)
+        self.numero2 = Input(fontS=40, fore="magenta", back="black")
+        layout.addWidget(self.numero2)
+        boton = Button('Sumar', back="orange", fontF="Chiller", fontS=50)
+        layout.addWidget(boton)
+        self.resultado = Text(fontS=60)
+        layout.addWidget(self.resultado)
+        boton.setDefault(True)
+        boton.clicked.connect(self.sumar)
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+
+    def sumar(self): # SLOT: se dispara cuando se produce el evento de click del botón
+        n1 = int(self.numero1.text())
+        n2 = int(self.numero2.text())
+        suma = str(n1 + n2)
+        self.resultado.setText(f"La suma es {suma}")
+
+if __name__ == '__main__':
+    app = QApplication()
+    window = MainWindow()
+    window.show()
+    app.exec()
---
+++ clases/posYtam.py
@@ -0,0 +1,30 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
+from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        self.texto = QLabel('label')
+        layout.addWidget(self.texto)
+        self.entrada = QLineEdit()
+        layout.addWidget(self.entrada)
+        boton = QPushButton('label')
+        layout.addWidget(boton)
+        boton.clicked.connect(self.function_name)
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+
+    def function_name(self):
+        self.texto.setText(f'Hola {self.entrada.text()}')
+
+if __name__ == '__main__':
+    app = QApplication()
+    window = MainWindow()
+    #window.resize(1000, 500) # cambiar tamaño
+    window.setGeometry(100, 200, 1000, 500) # posición y tamaño
+    window.show()
+    app.exec()
