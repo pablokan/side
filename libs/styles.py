8f80cba801d071140718b3db6462427db20433f3
Pablo Kaniefsky <pablokan@gmail.com>
2022-09-30 17:48:27 -0300

    styles ref 2

--- libs/styles.py
+++ libs/styles.py
@@ -1,12 +1,22 @@
+from dataclasses import dataclass
+
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
 from PySide6.QtGui import QFont
 from PySide6.QtCore import Qt
 
 
-class Style():
-    def __init__(self, text="", fontF="Arial", fontS=15, fore="black", 
-                back="white", horizontalAlign="hC", verticalAlign="vC",
-                moreStyle=""):
+@dataclass
+class Widget:
+    text: str = ""
+    fontF: str = "Arial"
+    fontS: int = 15
+    foreg: str = "black"
+    backg:str = "white"
+    hAlign: str ="hC"
+    vAlign: str ="vC"
+    moreStyle: str =""    
+
+    def style(self):
         align = {
             "aL": Qt.AlignLeft,
             "hC": Qt.AlignCenter,
@@ -15,46 +25,36 @@ class Style():
             "vC": Qt.AlignVCenter,
             "aB": Qt.AlignBottom
             }
-        self.setText(str(text))
+        self.setText(str(self.text))
         style = f"""
-            background-color: {back}; 
-            color: {fore};
+            background-color: {self.backg}; 
+            color: {self.foreg};
             padding: 5; 
-            
         """
-        self.setStyleSheet(style + moreStyle)
+        self.setStyleSheet(style + self.moreStyle)
         if not isinstance(self, QPushButton):
-            self.setAlignment(align[horizontalAlign] | align[verticalAlign])
-        self.setFont(QFont(fontF, fontS))
+            self.setAlignment(align[self.hAlign] | align[self.vAlign])
+        self.setFont(QFont(self.fontF, self.fontS))
 
-class Button(QPushButton, Style):
-    def __init__(self, text="", fontF="Arial", fontS=15, fore="black", 
-                back="white", moreStyle=""):
-        QPushButton.__init__(self)
-        Style.__init__(self, text, fontF, fontS, fore, back, moreStyle)
-    
-class Input(QLineEdit, Style):
-    def __init__(self, text="", fontF="Arial", fontS=15, fore="black", 
-                back="white", horizontalAlign="hC", verticalAlign="vC", moreStyle=""):
-        QLineEdit.__init__(self)
-        Style.__init__(self, text, fontF, fontS, fore, back, horizontalAlign, verticalAlign, moreStyle)
-        
-class Text(QLabel, Style):
-    """
-    alignment = {
-            "aL": Qt.AlignLeft,
-            "hC": Qt.AlignCenter,
-            "aR": Qt.AlignRight,
-            "aT": Qt.AlignTop, 
-            "vC": Qt.AlignVCenter,
-            "aB": Qt.AlignBottom
-            }
-    """
-    def __init__(self, text="", fontF="Arial", fontS=15, fore="black", 
-                back="white", horizontalAlign="hC", verticalAlign="vC", moreStyle=""):
+
+@dataclass
+class Text(Widget, QLabel):
+    def __post_init__(self):
         QLabel.__init__(self)
-        Style.__init__(self, text, fontF, fontS, fore, back, horizontalAlign, verticalAlign, moreStyle)
+        self.style()
         
+@dataclass
+class Input(Widget, QLineEdit):
+    def __post_init__(self):
+        QLineEdit.__init__(self)
+        self.style()
+
+@dataclass
+class Button(Widget, QPushButton):
+    def __post_init__(self):
+        QPushButton.__init__(self)
+        self.style()
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -72,15 +72,15 @@ class MainWindow(QMainWindow):
             layout.addWidget(Text(t, "NovaMono", 20, "cyan", c, a[0], a[1]))
 
         b = Button("Botón", "Victor Mono", 30, "red", "orange")
-        layout.addWidget(Text("pablo", "Victor Mono", 10, "sky blue", "blue", "aR", "aT"))
-        layout.addWidget(Text("kan", "Victor Mono", 30, "red", "orange", "aL", "aB", "border: 3px solid"))
+        layout.addWidget(Text("pablo", "Victor Mono", 20, "sky blue", "blue", "aR", "aT"))
+        layout.addWidget(Text("kan", "Victor Mono", 20, "red", "orange", "aL", "aB", "border: 3px solid"))
         layout.addWidget(b)
         layout.addWidget(Text("Manchester City"))
-        layout.addWidget(Input(fore="red", back="yellow"))
-        layout.addWidget(Text("Liverpool", horizontalAlign="aR"))
+        layout.addWidget(Input(foreg="red", backg="yellow"))
+        layout.addWidget(Text("Liverpool", hAlign="aR"))
         layout.addWidget(Text("lo mismo", moreStyle='border: 5px solid;'))
         layout.addWidget(Input("lo mismo"))
-
+        
         centralWidget = QWidget()
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
@@ -88,7 +88,7 @@ class MainWindow(QMainWindow):
 if __name__ == '__main__':
     app = QApplication()
     window = MainWindow()
-    window.resize(300, 400)
+    window.resize(500, 800)
     window.show()
     app.exec()
 
