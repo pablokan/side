9ec34c890e269ed1bc752bfc3a001f9634f8216d
Pablo Kaniefsky <pablokan@gmail.com>
2022-10-03 00:48:21 -0300

    refactor styles

--- libs/styles.py
+++ libs/styles.py
@@ -20,14 +20,13 @@ class Widget:
     fontF: str = "Arial"
     fontS: int = 15
     foreg: str = "black"
-    backg:str = None
+    backg: str = "white"
     hAlign: str ="hC"
     vAlign: str ="vC"
-    border: str = "0"
-    radius: str = "15"
-    moreStyle: str =""    
+    border: int = 0
+    radius: int = 15
 
-    def style(self):
+    def style(self, klass):
         align = {
             "aL": Qt.AlignLeft,
             "hC": Qt.AlignCenter,
@@ -37,38 +36,51 @@ class Widget:
             "aB": Qt.AlignBottom
             }
         self.setText(str(self.string))
-        style = f"""
+
+        sts = fr"""
+            {klass} {{
             background-color: {self.backg}; 
             color: {self.foreg};
             padding: 5; 
             border: {self.border}px solid;
             border-radius: {self.radius}px;
+        }}
         """
-        self.setStyleSheet(style + self.moreStyle)
-        if isinstance(self, QLineEdit):
+        
+        if isinstance(self, Input):
             self.selectAll()
-        if not isinstance(self, QPushButton):
+        if isinstance(self, Button):
+            stf =  fr"""
+            {klass}:focus {{
+                outline: none;
+                font-weight: 900;
+            }}
+            """
+            sts += stf
+        else:
             self.setAlignment(align[self.hAlign] | align[self.vAlign])
+            
         self.setFont(QFont(self.fontF, self.fontS))
-
+        self.setStyleSheet(sts)
+       
 
 @dataclass
 class Text(Widget, QLabel):
     def __post_init__(self):
         QLabel.__init__(self)
-        self.style()
+        self.style("Text")
         
 @dataclass
 class Input(Widget, QLineEdit):
     def __post_init__(self):
         QLineEdit.__init__(self)
-        self.style()
+        self.style("Input")
 
 @dataclass
 class Button(Widget, QPushButton):
     def __post_init__(self):
         QPushButton.__init__(self)
-        self.style()
+        self.style("Button")
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -85,13 +97,14 @@ class MainWindow(QMainWindow):
         for c, a in zip(colorList, alineado):
             t = f"texto #{c}"
             layout.addWidget(Text(t, "NovaMono", 20, "cyan", c, a[0], a[1]))
-
-        b = Button("Botón", "Victor Mono", 30, "red", "orange", radius=40, moreStyle='height: 200px')
-        layout.addWidget(Text("pablo", "Victor Mono", 20, "grey", "blue", "aR", "aT"))
-        layout.addWidget(Text("kan", "Victor Mono", 20, "red", "orange", "aL", "aB", "border: 3px solid"))
-        layout.addWidget(b)
-        layout.addWidget(Text("Manchester City"))
         layout.addWidget(Input(foreg="red", backg="yellow"))
+        b = Button("Botón", "Victor Mono", 30, "red", "orange", radius=20)
+        layout.addWidget(b)
+        layout.addWidget(Text("pablo", "Ani", 20, "grey", "blue", "aR", "aT", radius=0))
+        layout.addWidget(Text("kan", "Victor Mono", 20, "red", "orange", "aL", "aB", 3))
+        
+        layout.addWidget(Text("Manchester City"))
+        
         layout.addWidget(Text("Liverpool", hAlign="aR"))
         layout.addWidget(Text("lo mismo", border=10))
         layout.addWidget(Input("lo mismo"))
@@ -103,7 +116,7 @@ class MainWindow(QMainWindow):
 if __name__ == '__main__':
     app = QApplication()
     window = MainWindow()
-    window.resize(500, 800)
+    window.resize(500, 1000)
     window.show()
     app.exec()
 
