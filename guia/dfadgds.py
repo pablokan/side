db1fc43101805d2c683a236f08d5ba12718de5a5
Pablo Kaniefsky <pablokan@gmail.com>
2022-10-13 22:46:43 -0300

    menu basico

---
+++ guia/dfadgds.py
@@ -0,0 +1,31 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
+from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        self.texto = QLabel('label')
+        layout.addWidget(self.texto)
+        self.entrada = QLineEdit()
+        layout.addWidget(self.entrada)
+        boton = QPushButton('label')
+        boton.setDefault(True)
+        layout.addWidget(boton)
+        boton.clicked.connect(self.function_name)
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
+
+    def function_name(self):
+        self.texto.setText(f'Hola {self.entrada.text()}')
+
+if __name__ == '__main__':
+    app = QApplication()
+    css = '*{font-size: 30px; background-color: darkblue; color: skyblue;}'
+    app.setStyleSheet(css)
+    window = MainWindow()
+    window.show()
+    app.exec()
