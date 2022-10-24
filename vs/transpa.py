015601d6a2772b43188430501593447485941844
Pablo Kaniefsky <pablokan@gmail.com>
2022-10-24 18:55:54 -0300

    transparent

--- vs/transpa.py
+++ vs/transpa.py
@@ -1,7 +1,4 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton
-from PySide6.QtGui import QFont
-from PySide6.QtCore import Qt
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
